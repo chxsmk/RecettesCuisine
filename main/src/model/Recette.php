ba84fabe3470f363a9ab3b9ffbb1cf1502820ec1
Chamora Samakon <21903735@etu.unicaen.fr>
2021-11-02 21:40:50 +0100

    Modification des paramètres de la classe et Ajout de fonctions complémentaires

--- main/src/model/Recette.php
+++ main/src/model/Recette.php
@@ -1,16 +1,26 @@
 <?php
     class Recette{
         public $nom;
+        public $prenom;
+        public $titre;
         public $recette;
 
-        public function __construct($nom, $recette) {
+        public function __construct($nom, $prenom, $titre, $recette) {
             $this->nom = $nom;
+            $this->prenom = $prenom;
+            $this->titre = $titre;
             $this->recette = $recette;
         }
 
         //Accesseurs
         public function getNom() { 
             return $this->nom; 
+        }
+        public function getPrenom() { 
+            return $this->prenom; 
+        }
+        public function getTitre() { 
+            return $this->titre; 
         }
         public function getRecette() { 
             return $this->recette; 
@@ -23,6 +33,18 @@
             $this->nom = $nom;
         }
 
+        public function setPrenom($prenom) {
+            if (!self::isPrenomValid($prenom))
+                throw new Exception("Le prenom n'est pas valide");
+            $this->prenom = $prenom;
+        }
+
+        public function setTitre($titre) {
+            if (!self::isTitreValid($titre))
+                throw new Exception("Le titre n'est pas valide");
+            $this->titre = $titre;
+        }
+
         public function setRecette($recette) {
             if (!self::isRecetteValid($recette))
                 throw new Exception("La recette n'existe pas");
@@ -33,6 +55,14 @@
         public static function isNomValid($nom) {
             return $nom !== "";
         }
+
+        public static function isPrenomValid($prenom) {
+            return $prenom !== "";
+        }
+
+        public static function isTitreValid($titre) {
+            return $titre !== "";
+        }        
 
         public static function isRecetteValid($recette) {
             return $recette !== "";
