20058d840cb8aa576299d6bdd84e407a11b63a98
Chamora Samakon <21903735@etu.unicaen.fr>
2021-11-02 21:42:58 +0100

    Ajout de la fonction search()

--- main/src/model/RecetteStorageMySQL.php
+++ main/src/model/RecetteStorageMySQL.php
@@ -2,7 +2,6 @@
 
     session_start();
     require_once("RecetteStorage.php");
-    //require_once("lib/ObjectFileDB.php");
 
     class RecetteStorageMySQL implements RecetteStorage{
         protected $db;
@@ -16,7 +15,7 @@
             $requete->execute([":id" => $id]);
             $resultat = $requete->fetch();
             if($resultat!==false){
-                $recette = new Recette($resultat['nom'], $resultat['recette']);
+                $recette = new Recette($resultat['nom'], $resultat['prenom'], $resultat['titre'], $resultat['recette']);
                 return $recette;
             }
             else{
@@ -31,19 +30,26 @@
         }
 
         public function create(Recette $recette){
-            $requete = $this->db->prepare('INSERT INTO recettes (nom, recette) VALUES (:nom, :recette)');
-            $requete->execute(array(":nom" => $recette->getNom(), ":recette" => $recette->getRecette()));
+            $requete = $this->db->prepare('INSERT INTO recettes (nom, prenom, titre, recette) VALUES (:nom, :prenom, :titre, :recette)');
+            $requete->execute(array(":nom" => $recette->getNom(), ":prenom" => $recette->getPrenom(), ":titre" => $recette->getTitre(), ":recette" => $recette->getRecette()));
             return $this->db->lastInsertId();
+        }
+
+        public function search($recherche){
+            $requete =  $this->db->prepare('SELECT * FROM recettes WHERE titre LIKE :titre ORDER BY id DESC');
+            $requete->execute([":titre" => "%" . $recherche . "%"]);
+            $resultat = $requete->fetchAll();
+            return $resultat;
         }
 
         public function delete($id){
             $requete = $this->db->prepare('DELETE FROM recettes WHERE id= :id');
-            return $requete->execute([":id" => $id]);;
+            return $requete->execute([":id" => $id]);
         }
 
         public function update(Recette $recette, $id) {
-            $requete = $this->db->prepare('UPDATE recettes SET nom= :nom, recette= :recette WHERE id= :id');
-            return $requete->execute(array(":nom" => $recette->getNom(), ":recette" => $recette->getRecette(), "id" => $id));
+            $requete = $this->db->prepare('UPDATE recettes SET nom= :nom, prenom= :prenom, titre= :titre, recette= :recette WHERE id= :id');
+            return $requete->execute(array(":nom" => $recette->getNom(), ":prenom" => $recette->getPrenom(), ":titre" => $recette->getTitre(), ":recette" => $recette->getRecette(), "id" => $id));
         }
 
         //vélifie si les données entrées par l'utilisateur sont dans la bd.
